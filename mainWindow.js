6813a3a90eb10fca520a9dea3f6c874618399f33
sdpoulose <sdpoulose@gmail.com>
2020-09-29 12:07:53 -0400

    added value difference calculation

--- mainWindow.js
+++ mainWindow.js
@@ -1,10 +1,23 @@
 function calculateRates() {
+    // grab values from form
     var price1 = document.getElementById("price1").value;
     var price2 = document.getElementById("price2").value;
     var diameter1 = document.getElementById("diameter1").value;
     var diameter2 = document.getElementById("diameter2").value;
-    document.write(price1);
-    document.write(price2);
-    document.write(diameter1);
-    document.write(diameter2);
+    var value1 = diameter1*diameter1/price1;
+    var value2 = diameter2*diameter2/price2;
+    let difference;
+    // calculate percentage difference
+    if (value1 > value2) {
+        difference = value1/value2;
+    }
+    else {
+        difference = value2/value1;
+    }
+    //document.write(price1);
+    //document.write(price2);
+    //document.write(diameter1);
+    //document.write(diameter2);
+    // display difference
+    document.write(difference);
 }
