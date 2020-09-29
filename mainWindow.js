c67bf646d2f98029abbcf854cb4589cb321f445d
sdpoulose <sdpoulose@gmail.com>
2020-09-29 11:55:04 -0400

    added javascript functionality to button and the ability to see form

---
+++ mainWindow.js
@@ -0,0 +1,10 @@
+function calculateRates() {
+    var price1 = document.getElementById("price1").value;
+    var price2 = document.getElementById("price2").value;
+    var diameter1 = document.getElementById("diameter1").value;
+    var diameter2 = document.getElementById("diameter2").value;
+    document.write(price1);
+    document.write(price2);
+    document.write(diameter1);
+    document.write(diameter2);
+}
