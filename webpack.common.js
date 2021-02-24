3ee1c775b3de7dc682c115efa73e1af1eed14f9b
sdpoulose <sdpoulose@gmail.com>
2021-02-23 23:29:17 -0500

    added image loader to webpack

--- webpack.common.js
+++ webpack.common.js
@@ -30,6 +30,10 @@ module.exports = {
                     'css-loader',
                     'sass-loader',
                 ],
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader'
             }
         ]
     },
