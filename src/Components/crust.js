c82d1a911ea9fa9bbc955bb14bf6b7956e804d41
sdpoulose <sdpoulose@gmail.com>
2021-02-24 00:00:46 -0500

    replaced realistic textures with minimalistic bg colors

---
+++ src/Components/crust.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Crust extends Component {
+
+    render() {
+        return (
+            <div className="crust">
+                <h1>Pizza Value Calculator</h1>
+            </div>
+        );
+    }
+}
+
+export default Crust;
--- src/Components/homePage.js
+++ src/Components/homePage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Crust from "./crust"
 
 class HomePage extends Component {
 
     render() {
         return (
             <div className="cheese">
+                <Crust />
             </div>
         );
     }
