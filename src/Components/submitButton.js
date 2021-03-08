f85cdd5d52dfdca4f35c5756a2c96d4edeaef5e5
sdpoulose <sdpoulose@gmail.com>
2021-03-07 23:28:14 -0500

    made submit a customized bootstrap button

--- src/Components/submitButton.js
+++ src/Components/submitButton.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class SubmitButton extends Component {
 
     render() {
         return (
             <div>
-                <button className="submit">Submit</button>
+                <button className="submit btn btn-lg btn-block m-2 btn-sucess">Submit</button>
             </div>
 
         );
